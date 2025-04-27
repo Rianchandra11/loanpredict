9c433acf8f6a23b0eef8976e13d43d8b8f5ca662
rianchandraa11 <rianchandra11@gmail.com>
2025-04-27 18:18:08 +0700

    Second Commit

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import joblib
 
 app = Flask(__name__)
 
-model     = joblib.load('./loan/model.pkl')
-encoders  = joblib.load('./loan/encoders.pkl')
+model     = joblib.load('./model.pkl')
+encoders  = joblib.load('./encoders.pkl')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,7 +24,7 @@ def index():
             'loan_amnt':   float(request.form['loanamnt']),
             'loan_intent' : request.form['loanintent'],
             'loan_int_rate' :float(request.form['loanrate']),
-            'loan_percent_income' :float(request.form['loaninc']),
+            'loan_percent_income' :round(float(request.form['income'])/float(request.form['loanamnt']),2),
             'cb_person_cred_hist_length' :float(request.form['cb']),
             'credit_score': int(request.form['cs']),
             'previous_loan_defaults_on_file' : request.form['prevl']
